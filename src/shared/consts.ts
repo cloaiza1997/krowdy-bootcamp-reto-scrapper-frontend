e6438c183ada2f76a03accedf1fdf44d96c07008
Cristian Loaiza <kla-1997@hotmail.com>
2022-04-06 13:31:34 -0500

    master: Prod Api

--- src/shared/consts.ts
+++ src/shared/consts.ts
@@ -1,4 +1,7 @@
-export const URL_API = "http://localhost:5000/";
+export const URL_API =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5000/"
+    : "https://reto-scrapper-backend.herokuapp.com/";
 
 export const URL_API_SCRAPPER = URL_API + "scrap/profiles";
 export const URL_API_PROFILES = URL_API + "profiles";
